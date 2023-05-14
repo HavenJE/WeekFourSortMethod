e95c92a320f8b1c0067a7aced8fd514b473b170b
Haven <haven.eldebaque@gmail.com>
2023-05-15 00:53:38 +1000

    Add project files.

---
+++ WeekFourSortMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Week 4 - Sort() method 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] randomNums = { 50, 15, 11, 40, 7, 215 };
+        string[] randomNames = { "Xena", "Cortiney", "George", "Omar" };
+
+        Array.Sort(randomNames);
+        for (int i = 0; i < randomNames.Length; i++)
+        {
+            // Console.WriteLine(randomNums[i]);
+            Console.WriteLine(randomNames[i]);
+        }
+        
+    }
+}
